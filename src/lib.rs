49e39940e0b31a31bad5b3c72b5f7db366cd5f65
Julien Graziano <julien@graziano.fr>
2023-04-22 22:51:33 +0200

    test: test_tick

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,39 @@ pub struct Universe {
 }
 
 impl Universe {
+    /// Set the width of the universe.
+    /// 
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width*self.height).map(|_| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    /// 
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width*height).map(|_| Cell::Dead).collect();
+    }
+
+    /// Get the dead and alive values of the entire universe.
+    /// 
+    /// Returns a slice of Cell values
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in the universe given a list of coordinates.
+    /// 
+    /// Coordinates are given as a list of (row, column) tuples.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
     fn get_index(&self, row: u32, col: u32) -> usize {
         (row*self.width + col) as usize
     }
