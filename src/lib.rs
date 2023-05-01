cea95b9f7b88efe6f6cd55ae1a85963329a9d996
Julien Graziano <julien@graziano.fr>
2023-05-01 14:30:27 +0200

    refactor: move all rendering code to render module

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,11 @@ extern crate web_sys;
 mod life_106_codec;
 mod rle_codec;
 
+mod render;
+
 pub use rle_codec::*;
 pub use life_106_codec::*;
+pub use render::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -267,59 +270,5 @@ impl Universe {
         }
 
         self.cells = cells;
-    }
-
-    pub fn render_str(&self) -> String {
-        let mut str = String::new();
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                match cell {
-                    Cell::Dead => str.push('◻'),
-                    Cell::Alive => str.push('◼'),
-                }
-            }
-            str.push('\n');
-        }
-        str
-    }
-
-    /// Render the universe to a canvas element.
-    pub fn render_to_context(
-        &self,
-        context: web_sys::CanvasRenderingContext2d,
-        theme: JsValue,
-    ) -> Result<(), JsError> {
-        let cell_size = match js_sys::Reflect::get(&theme, &"cellSize".into()) {
-            Ok(value) => value.as_f64().ok_or(JsError::new("cellSize should be a number"))?,
-            Err(_) => 5.0,
-        };
-
-        let alive_color =
-            js_sys::Reflect::get(&theme, &"aliveCell".into())
-                .unwrap_or(JsValue::from_str("#000000"));
-
-        let dead_color =
-            js_sys::Reflect::get(&theme, &"deadCell".into())
-                .unwrap_or(JsValue::from_str("#ffffff"));
-
-        context.begin_path();
-        for (idx, cell) in self.cells.iter().copied().enumerate() {
-            if cell == Cell::Alive {
-                context.set_fill_style(&alive_color);
-            } else {
-                context.set_fill_style(&dead_color);
-            }
-
-            let (col, row) = self.get_coordinates(idx);
-            context.fill_rect(
-                (col as f64)*(cell_size + 1.0) + 1.0,
-                (row as f64)*(cell_size + 1.0) + 1.0,
-                cell_size,
-                cell_size,
-            );
-        }
-        context.stroke();
-
-        Ok(())
     }
 }
---
+++ src/render.rs
@@ -0,0 +1,59 @@
+use wasm_bindgen::prelude::*;
+
+use crate::*;
+
+#[wasm_bindgen]
+impl Universe {
+pub fn render_to_string(&self) -> String {
+    let mut str = String::new();
+    for line in self.cells.as_slice().chunks(self.width as usize) {
+        for &cell in line {
+            match cell {
+                Cell::Dead => str.push('◻'),
+                Cell::Alive => str.push('◼'),
+            }
+        }
+        str.push('\n');
+    }
+    str
+}
+
+/// Render the universe to a canvas element.
+pub fn render_to_context(
+    &self,
+    context: web_sys::CanvasRenderingContext2d,
+    theme: JsValue,
+) -> Result<(), JsError> {
+    let cell_size = match js_sys::Reflect::get(&theme, &"cellSize".into()) {
+        Ok(value) => value.as_f64().ok_or(JsError::new("cellSize should be a number"))?,
+        Err(_) => 5.0,
+    };
+
+    let alive_color =
+        js_sys::Reflect::get(&theme, &"aliveCell".into())
+            .unwrap_or(JsValue::from_str("#000000"));
+
+    let dead_color =
+        js_sys::Reflect::get(&theme, &"deadCell".into())
+            .unwrap_or(JsValue::from_str("#ffffff"));
+
+    context.begin_path();
+    for (idx, cell) in self.cells.iter().copied().enumerate() {
+        if cell == Cell::Alive {
+            context.set_fill_style(&alive_color);
+        } else {
+            context.set_fill_style(&dead_color);
+        }
+
+        let (col, row) = self.get_coordinates(idx);
+        context.fill_rect(
+            (col as f64)*(cell_size + 1.0) + 1.0,
+            (row as f64)*(cell_size + 1.0) + 1.0,
+            cell_size,
+            cell_size,
+        );
+    }
+    context.stroke();
+
+    Ok(())
+}}
